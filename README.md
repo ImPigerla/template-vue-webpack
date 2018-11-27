394d969abc0d0ac76fef5420d56286546bcd730a
wjj <474957860@qq.com>
2018-11-27 11:43:20 +0800

    vue-webpack项目的模版初始化

---
+++ README.md
@@ -0,0 +1,23 @@
+# 安装vue-cli
+$ npm install -g @vue/cli  或者  yarn global add @vue/cli
+
+## 安装init桥接工具
+npm install -g @vue/cli-init  或者  yarn global add @vue/cli-init
+
+
+# 获取模版  -template 支持选项
+#  |-Project name 项目名称
+$ vue init https://github.com/ImPigerla/template-vue-webpack [project-name]
+
+# 初始化
+$ cd project-name
+$ npm install 或者 yarn
+
+# 开发模式
+$ npm run dev
+
+# 构建项目
+$ npm run build
+
+# 构建监听
+$ npm run watch
---
+++ meta.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "helpers": {},
+    "prompts": {
+        "name": {
+            "type": "string",
+            "required": true,
+            "message": "Project name"
+        }
+    },
+    "filters": {
+        "src/i18n/**/*": "i18n",
+        "src/views/login.vue": "login",
+        "src/images/login.jpg": "login"
+    },
+    "skipInterpolation": [
+        "src/**/*.vue",
+        "*index.html"
+    ],
+    "completeMessage": "开始项目:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}npm install\n  npm run dev"
+};
